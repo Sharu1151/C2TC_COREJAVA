a4da78e7c84132247acc019ac2133a05bbbc3360
MSI <MSI@SHARATH>
2024-01-24 17:38:47 +0530

    master

---
+++ Day05/src/TNS/DAY5/day05_2.java
@@ -0,0 +1,15 @@
+package TNS.DAY5;
+
+public class day05_2 {
+private int rollNo;
+private String name;
+
+
+	
+public day05_2(int rollNo, String name) {
+	super();
+	this.rollNo = rollNo;
+	this.name = name;
+}
+
+}
---
+++ Day05/src/TNS/DAY5/demo05.java
@@ -0,0 +1,13 @@
+package TNS.DAY5;
+
+public class demo05 {
+	void add(int a,float b) {
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
